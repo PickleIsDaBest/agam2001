e367313dd8978472bb7659a39a7684b71618caf5
Agam Riyandana <87102026+agamx@users.noreply.github.com>
2022-10-10 14:57:12 +0700

    Delete visitor.js

--- visitor.js
+++
@@ -1,7 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.countapi.xyz/hit/agamriyandana.my.id/visits");
-xhr.responseType = "json";
-xhr.onload = function() {
-  document.querySelector('.footer-title').innerHTML = 'Total Visitor : ' + this.response.value;
-}
-xhr.send();
